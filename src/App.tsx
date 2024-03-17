780c9725973091ab5efca16d500291dd09524b24
Leandro Frederico <leandro_frederico@hotmail.com>
2024-03-17 20:01:17 -0300

    easter egg

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Viva o culto do sapo!
       </p>
     </>
   )
